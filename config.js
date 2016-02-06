20f1901780c427afe67d607ed69cb584cba8f2e3
Jonas Veselka <jonas.jonas.jonas@gmail.com>
2016-02-06 06:13:21 +0100

    config for production

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	mongoUrl: 'mongodb://localhost:27017/test',
 
-	layerCollectionName: "areatemplate2",
+	layerCollectionName: "areatemplate",
 	datasetCollectionName: "dataset",
 
 	resultAttributeName: "layerType",
